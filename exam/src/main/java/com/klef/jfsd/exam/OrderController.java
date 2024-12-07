5513fdb97d451d6456684f404814e3939723dbb2
Hemanth <2200030870cseh@gmail.com>
2024-12-07 08:12:48 +0530

    Add files via upload

---
+++ exam/src/main/java/com/klef/jfsd/exam/OrderController.java
@@ -0,0 +1,28 @@
+package com.klef.jfsd.exam;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Order> addOrder(@RequestBody Order order) {
+        if (order.getOrderId() == null || order.getOrderId().isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Order savedOrder = orderService.saveOrder(order);
+        return new ResponseEntity<>(savedOrder, HttpStatus.CREATED);
+    }
+   /* @GetMapping("/add-order-form")
+    public String showOrderForm() {
+        return "add-order";
+    }*/
+}
